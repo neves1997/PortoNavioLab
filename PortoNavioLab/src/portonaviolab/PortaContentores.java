9d543221dd6a508f0dc47d8e71bdf4e259e4e1e5
neves1997 <deandrefilipe@gmail.com>
2021-03-10 19:03:35 +0000

    branch _portacontentores

--- PortoNavioLab/src/portonaviolab/PortaContentores.java
+++ PortoNavioLab/src/portonaviolab/PortaContentores.java
@@ -12,5 +12,24 @@ package portonaviolab;
  */
 
 public class PortaContentores {
+   
+      public int num_max_contentores;
+
+      public PortaContentores (String matricula) {
+            super(matricula);//passa parâmetro para o construtor da superclasse 
+      }
+
+    public int getNum_max_contentores() {
+        return num_max_contentores;
+        //apenasumcomentrario
+    }
+
+    public void setNum_max_contentores(int num_max_contentores) {
+        this.num_max_contentores = num_max_contentores;
+        //alteraçãofeita
+    } 
     
+    public String toString() {
+        return "Porta-Contentores{" + "Matricula= " + matricula + ", Nome= " + nome + ", Comprimento= " + comprimento + ", Número Máximo de Contentores= "+ num_max_contentores +'}';
+    } 
 }
